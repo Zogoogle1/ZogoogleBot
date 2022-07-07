be47b1397b6a403822b36f89fafff1813cefbffd
Herbert40 <108845744+Herbert40@users.noreply.github.com>
2022-07-07 21:17:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,14 +19,14 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['+918416093656']
+global.premium = ['+918416093656']
+global.ownernomer = '+918416093656'
+global.ownername = 'Herbert Suntak'
+global.botname = 'HBWABot'
+global.footer = '©Herbert-inc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'India, Northeast India, Mizoram, Aizawl'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
